433c6b7a55b3d5c6c17192fa282c148f981e7e31
Taddes Korris <taddes.korris@gmail.com>
2019-03-23 15:25:41 -0400

    assets

--- todo-vuetify/src/views/Home.vue
+++ todo-vuetify/src/views/Home.vue
@@ -1,13 +1,13 @@
 <template>
-  <HelloWorld />
+  <div class="home">
+    <h1>Home Page</h1>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+
 
   export default {
-    components: {
-      HelloWorld
-    }
+
   }
 </script>
