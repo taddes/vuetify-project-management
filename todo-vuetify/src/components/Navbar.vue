2e29ca1d9b79dee0c3a947f9108f8a0ed5f2a142
Taddes Korris <taddes.korris@gmail.com>
2019-03-25 09:54:03 -0400

    avatar and profile spec

--- todo-vuetify/src/components/Navbar.vue
+++ todo-vuetify/src/components/Navbar.vue
@@ -14,6 +14,14 @@
   </v-toolbar>
 
   <v-navigation-drawer v-model="drawer" app class="warning">
+    <v-layout column align-center>
+      <v-flex class="mt-5">
+        <v-avatar size="100">
+          <img src="/tom.jpg" alt="">
+        </v-avatar>
+        <p></p>
+      </v-flex>
+    </v-layout>
     <v-list>
       <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
         <v-list-tile-action>
--- todo-vuetify/src/views/Team.vue
+++ todo-vuetify/src/views/Team.vue
@@ -4,11 +4,13 @@
 
     <v-container class="my-5">
 
-      <v-layout row wrap v-for="person in team" :key="person.name">
-        <v-flex xs12 sm6 md4 lg3>
+      <v-layout row wrap >
+        <v-flex xs12 sm6 md4 lg3 v-for="person in team" :key="person.name">
           <v-card flat class="text-xs-center ma-3">
             <v-responsive class="pt-4">
-              img goes here
+              <v-avatar size="100" class="grey lighten-3">
+                <img :src="person.avatar" alt="person.name">
+              </v-avatar>
             </v-responsive>
             <v-card-text>
               <div class="subheading">{{ person.name }}</div>
@@ -33,11 +35,12 @@
     data() {
       return {
         team : [
-          { 'name': 'Emily', 'role': 'Back-end Dev'},
-          { 'name': 'Francis', 'role': 'UI-UX Dev'},
-          { 'name': 'Tom', 'role': 'Back-end Dev'},
-          { 'name': 'Sarah', 'role': 'Back-end Dev'},
-          { 'name': 'Cameron', 'role': 'QA Dev'}
+          { 'name': 'Emily', 'role': 'Back-end Dev',  'avatar': '/emily.jpeg'},
+          { 'name': 'Francis', 'role': 'UI-UX Dev', 'avatar': '/francis.jpg'},
+          { 'name': 'Tom', 'role': 'Back-end Dev', 'avatar': '/tom.jpg'},
+          { 'name': 'Sarah', 'role': 'Back-end Dev', 'avatar': '/sarah.jpg'},
+          { 'name': 'Cameron', 'role': 'QA Dev', 'avatar': '/cameron.jpg'},
+          { 'name': 'Jeremy', 'role': 'Project Manager', 'avatar': '/jeremy.jpg'}
         ]
       }
     },
